139f86df77010e8202a04b4894e045512aa8c20b
Ws-Peroth <a01033056809@gmail.com>
2021-04-02 16:39:28 +0900

    add error code
    add error code

--- Firebase Auth Test/Assets/Scripts/AuthTest.cs
+++ Firebase Auth Test/Assets/Scripts/AuthTest.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Firebase.Auth; // 계정인증기능 사용
 
 public class AuthTest : MonoBehaviour
 {
+    public Text errorText;
     private FirebaseAuth auth; // 인증 객체 불러오기
 
     void Start()
     {
         auth = FirebaseAuth.DefaultInstance; // 인증 객체 초기화
         Join("tester@gmail.com", "password"); // 해당 이메일,비밀번호로 가입하기
+        // Login("tester@gmail.com", "password");
         print("E-Mail : tester@gmail.com, \nPw = password");
     }
 
@@ -23,6 +26,7 @@ public class AuthTest : MonoBehaviour
                  if (!task.IsCanceled && !task.IsFaulted)
                  {
                      Debug.Log(email + " 로 회원가입 하셨습니다.");
+                     errorText.text = email + " 로 회원가입 하셨습니다.";
                  }
                  else
                  {
@@ -35,16 +39,7 @@ public class AuthTest : MonoBehaviour
                      if (task.IsFaulted)
                      {
                          string errorCode = task.Exception.ToString();
-                         print(errorCode);
-
-                         string result = errorCode.Split(
-                                new string[] { "Firebase.FirebaseException:" }, 
-                                System.StringSplitOptions.None)[1];
-
-                         result = result.Split('\n')[0];
-
-                         print(result);
-
+                         PrintErrorCode(errorCode);
                          return;
                      }
                      
@@ -52,5 +47,40 @@ public class AuthTest : MonoBehaviour
              }
          );
     }
+    
+    void PrintErrorCode(string errorCode)
+    {
+        print(errorCode);
 
+        string result = errorCode.Split(
+               new string[] { "Firebase.FirebaseException:" },
+               System.StringSplitOptions.None)[1];
+
+        result = result.Split('\n')[0];
+
+        print(result);
+        errorText.text = result;
+    }
+
+    void Login(string email, string password)
+    {
+        // 이메일과 비밀번호로 가입하는 함수
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(
+            task => {
+                if (task.IsCompleted && !task.IsFaulted && !task.IsCanceled)
+                {
+                    Debug.Log(email + " 로 로그인 하셨습니다.");
+                }
+                else
+                {
+                    Debug.Log("로그인에 실패하셨습니다.");
+                }
+            }
+        );
+    }
+
+    public void SetData()
+    {
+
+    }
 }
